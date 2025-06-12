756dffc3ca50984bd48e1265e98459085e480375
hiraaaku <muthiawidya.fahira@gmail.com>
2025-06-12 21:21:39 +0700

    feat: footer

--- components/footer.vue
+++ components/footer.vue
@@ -1,116 +1,220 @@
 <template>
   <footer>
-    <div class="tp-footer-area tp-footer-style-2 tp-footer-style-5" style="background-color: #f4f7f9" >
-      <div class="tp-footer-top pt-95 pb-45">
-          <div class="container">
-            <div class="row">
-                <div class="col-xl-4 col-lg-3 col-md-4 col-sm-6">
-                  <div class="tp-footer-widget footer-col-1 mb-50">
-                      <div class="tp-footer-widget-content">
-                        <div class="tp-footer-logo">
-                            <nuxt-link href="/">
-                              <img src="/images/logo/logo.svg" alt="logo">
-                            </nuxt-link>
-                        </div>
-                        <p class="tp-footer-desc">
-                          We are a team of designers and developers that create high quality WordPress
-                        </p>
-                        <div class="tp-footer-social">
-                            <social-links></social-links>
-                        </div>
-                      </div>
-                  </div>
-                </div>
-                <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
-                  <div class="tp-footer-widget footer-col-2 mb-50">
-                      <h4 class="tp-footer-widget-title">My Account</h4>
-                      <div class="tp-footer-widget-content">
-                        <ul>
-                            <li><a href="#">Track Orders</a></li>
-                            <li><a href="#">Shipping</a></li>
-                            <li><a href="#">Wishlist</a></li>
-                            <li><a href="#">My Account</a></li>
-                            <li><a href="#">Order History</a></li>
-                            <li><a href="#">Returns</a></li>
-                        </ul>
-                      </div>
-                  </div>
-                </div>
-                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6">
-                  <div class="tp-footer-widget footer-col-3 mb-50">
-                      <h4 class="tp-footer-widget-title">Infomation</h4>
-                      <div class="tp-footer-widget-content">
-                        <ul>
-                            <li><a href="#">Our Story</a></li>
-                            <li><a href="#">Careers</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">Terms & Conditions</a></li>
-                            <li><a href="#">Latest News</a></li>
-                            <li><a href="#">Contact Us</a></li>
-                        </ul>
-                      </div>
-                  </div>
-                </div>
-                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6">
-                  <div class="tp-footer-widget footer-col-4 mb-50">
-                      <h4 class="tp-footer-widget-title">Talk To Us</h4>
-                      <div class="tp-footer-widget-content">
-                        <div class="tp-footer-talk mb-20">
-                            <span>Got Questions? Call us</span>
-                            <h4><a href="tel:670-413-90-762">+670 413 90 762</a></h4>
-                        </div>
-                        <div class="tp-footer-contact">
-                            <div class="tp-footer-contact-item d-flex align-items-start">
-                              <div class="tp-footer-contact-icon">
-                                  <span>
-                                    <svg-email></svg-email>
-                                  </span>
-                              </div>
-                              <div class="tp-footer-contact-content">
-                                  <p><a href="mailto:shofi@gmail.com">shofi@gmail.com</a></p>
-                              </div>
-                            </div>
-                            <div class="tp-footer-contact-item d-flex align-items-start">
-                              <div class="tp-footer-contact-icon">
-                                  <span>
-                                    <svg-location></svg-location>
-                                  </span>
-                              </div>
-                              <div class="tp-footer-contact-content">
-                                  <p><a href="https://www.google.com/maps/place/Sleepy+Hollow+Rd,+Gouverneur,+NY+13642,+USA/@44.3304966,-75.4552367,17z/data=!3m1!4b1!4m6!3m5!1s0x4cccddac8972c5eb:0x56286024afff537a!8m2!3d44.3304928!4d-75.453048!16s%2Fg%2F1tdsjdj4" target="_blank">79 Sleepy Hollow St. <br> Jamaica, New York 1432</a></p>
-                              </div>
-                            </div>
-                        </div>
-                      </div>
-                  </div>
-                </div>
-            </div>
-          </div>
+    <div class="container">
+      <div class="about-us">
+        <p class="title">About Us.</p>
+        <p class="description">orporate clients and leisure travelers has been relying on Groundlink for dependable
+          safe, and professional
+          chauffeured car service in major cities across World. Indeed it has been more than one decade and five years
+          that Groundlink</p>
+        <div class="socials">
+          <a href="https://www.facebook.com/profile.php?id=100093111111111" target="_blank" rel="noopener noreferrer">
+            <Icon name="fa:facebook" />
+          </a>
+          <a href="https://www.twitter.com/profile.php?id=100093111111111" target="_blank" rel="noopener noreferrer">
+            <Icon name="mdi:twitter" />
+          </a>
+          <a href="https://vimeo.com/profile.php?id=100093111111111" target="_blank" rel="noopener noreferrer">
+            <Icon name="entypo-social:vimeo" />
+          </a>
+          <a href="https://www.linkedin.com/profile.php?id=100093111111111" target="_blank" rel="noopener noreferrer">
+            <Icon name="streamline-logos:path-logo-solid" />
+          </a>
+        </div>
       </div>
-      <div class="tp-footer-bottom">
-          <div class="container">
-            <div class="tp-footer-bottom-wrapper">
-                <div class="row align-items-center">
-                  <div class="col-md-6">
-                      <div class="tp-footer-copyright">
-                        <p>© {{new Date().getFullYear()}} All Rights Reserved  |  Vue nuxt Template by <nuxt-link href="/">Themepure</nuxt-link>.</p>
-                      </div>
-                  </div>
-                  <div class="col-md-6">
-                      <div class="tp-footer-payment text-md-end">
-                        <p>
-                            <img src="/images/footer/footer-pay-2.png" alt="">
-                        </p>
-                      </div>
-                  </div>
-                </div>
-            </div>
-          </div>
+      <div class="contact">
+        <h2>PT Agro Asia Berdikari</h2>
+        <a href="mailto:agroasia119@gmail.com" class="contact__mail">agroasia119@gmail.com</a>
+        <p class="contact__phone">+62 812 773 3166 | +62 21 74634590</p>
+        <div class="contact-address">
+          <Icon name="mdi:map-marker-outline" class="contact-address-icon" />
+          <span>Jl Palapa perum serua barokah Blok D12 serua Tangerang selatan 15414</span>
+        </div>
+        <a class="wa-button" href="https://wa.me/628127733166" target="_blank" rel="noopener noreferrer">
+          <Icon name="mdi:whatsapp" class="wa-button-title" />
+          <p class="wa-button-item wa-button__text">+62 812 773 3166</p>
+        </a>
       </div>
+    </div>
+    <div class="copyright">
+      <p>© Agro Asia Berdikari 2025 All rights reserved.</p>
     </div>
   </footer>
 </template>
 
 <script setup lang="ts">
-defineProps<{bg_clr?:boolean}>();
+defineProps<{ bg_clr?: boolean }>();
 </script>
+
+<style lang="scss" scoped>
+@use '@/assets/scss/utils/_colors.scss' as *;
+@use '@/assets/scss/utils/_breakpoints.scss' as *;
+
+footer {
+  background-color: map-get($colors, 'black', 'indigo');
+
+  .container {
+    max-width: 1200px;
+    width: 100%;
+    margin: 0 auto;
+    padding: 8rem 1rem 2rem 1rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 5rem;
+
+    @media #{$xs} {
+      flex-direction: column;
+      align-items: start;
+    }
+  }
+}
+
+.about-us {
+  max-width: 300px;
+}
+
+.socials {
+  display: flex;
+  gap: 1rem;
+
+  a {
+    height: 40px;
+    width: 40px;
+    background-color: map-get($colors, 'white', 'pure');
+    border-radius: 100%;
+    color: map-get($colors, 'black', 'indigo');
+    font-size: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: all 0.3s ease;
+
+    &:hover {
+      background-color: map-get($colors, 'green', 'light');
+      color: map-get($colors, 'white', 'pure');
+    }
+  }
+}
+
+footer p {
+  font-family: 'Nunito', sans-serif;
+  color: map-get($colors, 'white', 'pure');
+
+  &.title {
+    font-size: 20px;
+    font-weight: bold;
+    line-height: 32px;
+    margin-bottom: 1rem;
+  }
+
+  &.description {
+    font-size: 16px;
+    font-weight: 400;
+    margin-bottom: 1rem;
+  }
+}
+
+.copyright {
+  background-color: map-get($colors, 'grey', 'chaorcal');
+
+  p {
+  text-align: left;
+  font-size: 14px;
+  color: map-get($colors, 'white', 'pure');
+  max-width: 1200px;
+  margin-bottom: 0;
+  padding: .8rem 1rem;
+  margin: 0 auto;
+  }
+}
+
+.contact {
+  max-width: 400px;
+  width: 100%;
+
+  h2 {
+    color: map-get($colors, 'white', 'pure');
+    font-size: 34px;
+    font-weight: bold;
+    margin-bottom: 1rem;
+  }
+
+  &__mail,
+  &__phone,
+  &__address {
+    color: map-get($colors, 'white', 'pure');
+    font-size: 16px;
+    margin-bottom: .5rem;
+    display: block;
+  }
+
+  &__address {
+    margin-bottom: 1rem;
+  }
+
+  &-address {
+    display: flex;
+    align-items: start;
+    justify-content: start;
+    gap: 1rem;
+    font-size: 16px;
+    color: map-get($colors, 'white', 'pure');
+
+    &-icon {
+      font-size: 30px;
+      width: 30px;
+      display: block;
+    }
+  }
+
+}
+
+.wa-button {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  background-color: map-get($colors, 'white', 'pure');
+  color: map-get($colors, 'black', 'indigo');
+  margin-top: 2rem;
+  padding: .3rem;
+  border-radius: 5px;
+
+  &-title {
+    font-size: 24px;
+    color: map-get($colors, 'green', 'light');
+    margin-left: .8rem;
+  }
+
+  &-item {
+    font-size: 16px;
+    background-color: map-get($colors, 'green', 'light');
+    width: 100%;
+    height: 100%;
+    margin-bottom: 0;
+    font-size: 12px;
+    padding: .5rem 1rem;
+    border-radius: 10px;
+
+    &:hover {
+      background-color: map-get($colors, 'green', 'medium');
+      color: map-get($colors, 'white', 'pure');
+    }
+  }
+
+  &__text {
+    font-size: 16px;
+
+    a {
+      font-size: 16px;
+      color: map-get($colors, 'white', 'pure');
+      text-decoration: none;
+      margin-bottom: 0;
+
+      p {
+        margin: 0;
+      }
+    }
+  }
+}
+</style>
