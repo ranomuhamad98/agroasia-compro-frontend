6b2fa6fdc7d3b1292a79a50f3146622357625d56
hiraaaku <muthiawidya.fahira@gmail.com>
2025-06-16 20:40:11 +0700

    feat: video & petani kami on about us page

---
+++ components/about/about-with-video.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="mb-100">
+        <video src="https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+            poster="/public/images/about/video-thumbnail.png" loop playsinline controls class="tp-about-video"></video>
+    </div>
+</template>
+
+<style scoped>
+.tp-about-video {
+    width: 100vw;
+    height: 100%;
+}
+</style>
--- pages/about.vue
+++ pages/about.vue
@@ -3,25 +3,11 @@
     <breadcrumb-with-image title="About Us" :subtitle="['Home','About Us']" color="var(--tp-common-white)" />
     
     <about-know-more />
-    <!-- about area start -->
-    <!-- <about-area/> -->
-    <!-- about area end -->
 
-    <!-- counter area start -->
-    <!-- <counter-area/>
-    <!-- counter area end -->
+    <about-with-video />
 
-    <!-- history area start -->
-    <!-- <history-area/> -->
-    <!-- history area end -->
+    <about-petani-kami />
 
-    <!-- work area start -->
-    <!-- <work-area/> -->
-    <!-- work area end -->
-
-    <!-- author area start -->
-    <!-- <author-area/> -->
-    <!-- author area end -->
   </div>
 </template>
 
